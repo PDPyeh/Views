4e71e8e80d9ebb4bdc16e00a5026f8e4334b2e85
Pradipa Y <pdpyyo@gmail.com>
2025-11-06 16:48:47 +0700

    menambahkan NavHost di Scaffhold function DataApp di Navigasi.kt

--- app/src/main/java/com/example/views/Navigasi.kt
+++ app/src/main/java/com/example/views/Navigasi.kt
@@ -1,9 +1,14 @@
 package com.example.views
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.views.view.FormIsian
 
 enum class Navigasi {
     Formulirku,
@@ -15,5 +20,19 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
 
+            modifier = Modifier.padding(paddingValues = isiRuang)){
+            composable(route=Navigasi.Formulirku.name){
+                FormIsian (
+                    pilihanJK = JenisK.map {id -> konteks.resource.getString(id)}
+                    onSubmitBtnClick =
+                )
+            }
+
+        }
+    }
 }
