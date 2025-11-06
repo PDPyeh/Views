52dcc01cd12853b7bd38190eb08fac210244d96b
Pradipa Y <pdpyyo@gmail.com>
2025-11-06 16:42:20 +0700

    menambahkan function DataApp di Navigasi.kt

--- app/src/main/java/com/example/views/Navigasi.kt
+++ app/src/main/java/com/example/views/Navigasi.kt
@@ -1,6 +1,19 @@
 package com.example.views
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+
 }
