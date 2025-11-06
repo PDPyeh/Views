5a367f623026e47175b14735082a1daaac70adec
Pradipa Y <pdpyyo@gmail.com>
2025-11-06 15:51:25 +0700

    mengisi isiRuang dengan OutlinedTextField di Column di FormIsian

--- app/src/main/java/com/example/views/view/Formulir.kt
+++ app/src/main/java/com/example/views/view/Formulir.kt
@@ -3,6 +3,8 @@ package com.example.views.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.views.R
 
 @Composable
@@ -34,6 +37,15 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
             ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
 
         }
 
