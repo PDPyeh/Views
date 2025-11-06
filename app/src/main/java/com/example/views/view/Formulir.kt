b9c4820494a366bb05e75a4d3bed0ad649ee4e69
Pradipa Y <pdpyyo@gmail.com>
2025-11-06 16:04:24 +0700

    mengisi isiRuang dengan HorizontalDivider2 di Column di FormIsian

--- app/src/main/java/com/example/views/view/Formulir.kt
+++ app/src/main/java/com/example/views/view/Formulir.kt
@@ -69,7 +69,13 @@ fun FormIsian(
 
                 }
             }
-            HorizontalDivider()
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
         }
 
     }
